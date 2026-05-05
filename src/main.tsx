e644670d0a50d377ea8bc62962bc267da2ceb0de
Sergio Ahumada <sergio.ahumada.m@gmail.com>
2026-05-04 23:50:15 -0400

    performance

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import '@tensorflow/tfjs-backend-webgl';
+import * as tf from '@tensorflow/tfjs';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
+tf.setBackend('webgl');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
